2f18efbdcfbc980759e387f4d14246ebf7ee9e84
jorgemochonbernalpersonal <jorgemochonbernalgitpersonal@gmail.com>
2026-01-03 23:53:22 +0100

    p

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,17 @@
 <?php
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     
     // Clinora Modular Service Providers
     App\Providers\CoreServiceProvider::class,
     App\Providers\PsychologyModuleServiceProvider::class,
     App\Providers\PatientPortalServiceProvider::class,
 ];
+
+// Only register Telescope if it's installed
+if (class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class)) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
