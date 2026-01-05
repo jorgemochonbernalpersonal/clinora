2e58bc5223c43f7bd9ca9ebf1f22c99928b79cf2
jorgemochonbernalpersonal <jorgemochonbernalgitpersonal@gmail.com>
2026-01-05 11:44:50 +0100

    o

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -3,8 +3,7 @@ use App\Shared\Helpers\SitemapHelper;
 $entries = $entries ?? SitemapHelper::getSitemapEntries();
 @endphp
 <?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="{{ url('/sitemap.xsl') }}"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
 @foreach($entries as $entry)
 @if(!empty($entry['url']))
 <url>
